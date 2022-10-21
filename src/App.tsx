c10c2a5b26b2d76299f487fd82c37d3407f3b5b7
ivettepinto7 <00186319@uca.edu.sv>
2022-10-21 14:42:20 -0600

    :white_check_mark: add frontend tests

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import duiValidator from './duiValidator';
 
 function App() {
   const [value, setValue] = useState("");
+  const [mensaje, setMensaje] = useState("");
 
   const onSubmit = () => {
     if(duiValidator(value)){
-      console.log("ES TRUE");
+      setMensaje("DUI VALIDO");
     }else {
-      console.log("NO ES TRUE");
+      setMensaje("DUI INVALIDO");
     }
   }
 
   return (
     <div className='relative bg-slate-300 w-full min-h-screen flex items-center justify-center flex-col'>
       <div className='w-full max-w-lg text-center flex flex-col gap-16'>
-        <h1 className='text-5xl font-bold'>¿Mi DUI es válido?</h1>
+        <h1 className='text-5xl font-bold'>¿Mi DUI es valido?</h1>
+        <span className={`${mensaje === 'DUI INVALIDO' ? 'text-red-500' : 'text-blue-500'}`}>{mensaje}</span>
         <form
           className='flex flex-col gap-5 items-center'
           onSubmit={(e) => {
@@ -26,6 +28,7 @@ function App() {
           <input
             placeholder='000000000'
             className='rounded p-2 w-full'
+            data-testid="dui-input"
             value={value}
             onChange={(e ) => setValue(e.target.value)}
           />
